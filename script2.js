d7208614f17b0f0d693af6161914ed8b0813c67d
ZORO <pixeldomainbusiness@gmail.com>
2025-01-18 19:10:54 +0530

    update3

---
+++ script2.js
@@ -0,0 +1,21 @@
+    (function() {
+        emailjs.init("WR-oGVCgHzD74rs1v"); 
+    })();
+
+    document.getElementById("contact-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const serviceID = "service_qtve8iq";
+        const templateID = "template_77aqy2b";
+
+        const form = this;
+
+        emailjs.sendForm(serviceID, templateID, form)
+            .then(() => {
+                alert("Message sent successfully!");
+                form.reset();
+            }, (err) => {
+                alert("Failed to send message. Please try again later.");
+                console.error("EmailJS error:", err);
+            });
+    });
